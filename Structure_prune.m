477db34a0695141702b82d35cdcf54d20f0f6d9a
yooceii <yooceii19960620@gmail.com>
2018-03-20 11:40:14 -0300

    add structure_prune matlab file

---
+++ Structure_prune.m
@@ -0,0 +1,17 @@
+function y=Structure_prune(k)
+data=csvread('export.csv',1,1);
+[Idx,Ctrs] = kmeans(data,k);
+closestIdx=zeros(1,k,'uint16');
+for iCluster = 1:max(Idx)
+    %# find the points that are part of the current cluster
+    currentPointIdx = find(Idx==iCluster);
+    %# find the index (among points in the cluster)
+    %# of the point that has the smallest Euclidean distance from the centroid
+    %# bsxfun subtracts coordinates, then you sum the squares of
+    %# the distance vectors, then you take the minimum
+    [~,minIdx] = min(sum(bsxfun(@minus,data(currentPointIdx,:),Ctrs(iCluster,:)).^2,2));
+    %# store the index into X (among all the points)
+    closestIdx(iCluster) = currentPointIdx(minIdx);
+end
+y=closestIdx;
+end
